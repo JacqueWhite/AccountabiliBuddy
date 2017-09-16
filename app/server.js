835fc3cfa459bd6e2134ec30de3ab061d80c429b
JacqueWhite <jacquecwhite@gmail.com>
2017-09-16 18:33:44 -0500

    hopefully fixing server bug

--- app/server.js
+++ app/server.js
@@ -6,8 +6,8 @@ var path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
 var PORT = process.env.PORT || 3000;
+var app = express();
 
 // BodyParser
 app.use(bodyParser.json());
@@ -20,7 +20,7 @@ app.use(express.static("./app/public"));
 
 app.use(bodyParser.text());
 app.use(bodyParser.json({ 
-	type: "application/vnd.api+json" 
+	type: "application/vnd.api.json" 
 }));
 
 // the main client pages
@@ -30,5 +30,5 @@ require("./app/routing/apiRoutes.js")(app);
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("server listening on %d", PORT);
 });
