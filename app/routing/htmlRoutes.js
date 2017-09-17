b96d1c98a516803c6f7c1468691d33837e46bcff
JacqueWhite <jacquecwhite@gmail.com>
2017-09-17 14:21:16 -0500

    reversing file changes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,22 +3,19 @@
 var path = require('path');
 console.log(path);
 
-// client requests
-// default to home
+// Basic route that sends the user first to the AJAX Page
 module.exports = function(app) {
-	app.get("/", function(req, res) {
-	  res.sendFile(path.join(__dirname, "/../public/home.html"));
-	});
-
-	app.post("/", function(req, res) {
-	    console.log(req.body)
-	});
-
-	app.get("/survey", function(req, res) {
-	  res.sendFile(path.join(__dirname, "/../public/survey.html"));
-	});
-
-	app.post("/survey", function(req, res) {
-	        console.log(req.body)
-	});
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, '/../public/home.html'));
+    });
+    app.post('/', function(req, res) {
+        console.log(req.body)
+    });
+    // default to survey
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, '/../public/survey.html'));
+    });
+    app.post('/survey', function(req, res) {
+        console.log(req.body)
+    });
 }
